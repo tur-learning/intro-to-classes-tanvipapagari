bca3f5aca836d0ce05ca3c7ee2439d10d0f6057a
Tanvi Papagari <tanvi.papagari@temple.edu>
2025-02-13 17:07:05 +0000

    finally figured out the code

--- lab-sessions/classes/classes.py
+++ lab-sessions/classes/classes.py
@@ -1,27 +1,31 @@
 class Person:
-    def __init__ (self, name, age, height):
+    def __init__(self, name, age, height):
         self.name = name
         self.age = age
         self.height = height
 
-    def info  (self):
-        print (self.name, self.age, self.height)
-    def change_age (self, new_age):
+    def info(self):
+        print(self.name, self.age, self.height)
+
+    def change_age(self, new_age):
         self.age = new_age
 
 class Country:
-    def __init__ (self, name, continent, people, flowers):
+    def __init__(self, name, continent, people, flowers):
         self.name = name
         self.continent = continent
         self.people = people
         self.flowers = flowers
-
-    def print_info (self):
-        print (self.name, self.continent)
         
-    def change_people(self, new_people):
+    def print_info(self):
+        print(f"Country: {self.name}, Continent: {self.continent}, Population: {self.people}, Flowers: {self.flowers}")
+        
+    def change_people(self, new_people): 
         self.people = new_people
-    
-    
+
     def calculate_flowers(self):
-        self.people_flowers = self.people / self.flowers
+        print("Flowers per Person:")
+        if self.flowers != 0:
+            print(f"{self.people / self.flowers} 🌼") 
+        else:
+            print("Cannot divide by zero :).")
--- lab-sessions/classes/main.py
+++ lab-sessions/classes/main.py
@@ -1,15 +1,14 @@
-from classes import Country
+from classes import Country  
 
-italy= Country ("Italy", "Europe", 55, 55)
-
-france= Country ("France", "Europe", 65, 55)
+italy = Country("Italy", "Europe", 55, 55)
+france = Country("France", "Europe", 65, 55)
 
 italy.print_info()
 france.print_info()
 
-france.change_people(70)
-france.print_info()
+italy.change_people(60)  
+print(f"Italy's new population: {italy.people}")
 
-print("Flowers per People:")
 italy.calculate_flowers()
 italy.print_info()
+
